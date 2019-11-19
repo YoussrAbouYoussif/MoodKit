a7f48ae2eb3b90391714644de7957fd5aa67755a
NadaLabib <nada.elazzab@student.guc.edu.eg>
2019-11-19 23:26:00 +0200

    modfication on Mood

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://youssrabouyoussif:Ya.12345@cluster0-ga4kc.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'verysecretkey'
+  mongoURI:
+    'mongodb+srv://youssrabouyoussif:Ya.12345@cluster0-ga4kc.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'verysecretkey'
 }
--- models/Question.js
+++ models/Question.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const moment = require('moment');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+//const moment = require('moment');
 
 const Question = new Schema({
-	questionName: { type: String, required: true },
-	answers: [{ type: String}]
-});
+  questionName: { type: String, required: true },
+  answers: [{ type: String }]
+})
 
-module.exports = question = mongoose.model('questions', Question);
+module.exports = question = mongoose.model('questions', Question)
